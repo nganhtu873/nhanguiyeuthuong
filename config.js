f4725cf4cb77199789308083b72ef8da9ae8afc1
tsu <ngtsu873@gmail.com>
2021-11-14 11:51:17 +0700

    file

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ const titleCard = 'Lời nhắn';
 const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
-const giftImage = 'hot-girl.png';
+const giftImage = 'hot-girl1.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
