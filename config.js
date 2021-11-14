acd8b1691f3915e7fe1f28acf94c2626e9883cd9
tsu <ngtsu873@gmail.com>
2021-11-14 12:25:05 +0700

    file

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const nameGirl = '...';
-const giftUrl = 'http://nodemy.vn';
+const giftUrl = '#';
 const eventName = 'Tâm thư gửi';
 const titleCard = 'Lời nhắn';
 const contentCard = 'chúc...';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
-const giftImage = 'hot-gir.png';
+const giftImage = 'hot-girl.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
